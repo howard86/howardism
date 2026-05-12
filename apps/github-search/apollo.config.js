2f3ab299fa9fc02c6c1e39997d757e0cbe9a3f24
Howard Tai <howard@howardism.dev>
2026-05-12 22:45:50 +0800

    :fire: chore: remove unused apps (github-search, minecraft, recipe, template)

--- apps/github-search/apollo.config.js
+++
@@ -1,7 +0,0 @@
-const { name } = require("./package.json");
-
-module.exports = {
-  client: {
-    service: name,
-  },
-};
--- apps/recipe/src/redux/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export type LocalAPIResponse<T = unknown> = T & { success: boolean };
-
-const api = axios.create({ baseURL: "/api" });
-
-export default api;
