8686d702b5997343a98f4ee3e426fe7981420ff5
Howard Tai <howard@howardism.dev>
2026-05-25 23:21:01 +0800

    feat(blog): install next-intl and add routing configuration

---
+++ apps/blog/src/i18n/routing.ts
@@ -0,0 +1,10 @@
+import { defineRouting } from "next-intl/routing";
+
+export const routing = defineRouting({
+  locales: ["en", "zh-TW"],
+  defaultLocale: "en",
+  localePrefix: "as-needed",
+});
+
+export type Locale = (typeof routing.locales)[number];
+export const DEFAULT_LOCALE = routing.defaultLocale;
